f3291570e92af02db594c7755ce58db1f7886242
oaaky <omkar.omkar.135@gmail.com>
2020-09-21 18:52:08 -0400

    minor change in setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# Reference :  Setup script documentation 
+# https://setuptools.readthedocs.io/en/latest/setuptools.html
+
+from setuptools import setup, find_packages
+
+def get_requirements(filename):
+    with open(filename) as f:
+        requirements = f.read().splitlines()
+    return requirements
+
+setup(name='codeTime',
+      version='1.0',
+      description='CSC 510: Software Engineering Project 1',
+      author='Omkar Kulkarni',
+      author_email='omkar.omkar.135@gmail.com',
+      license="MIT",
+      packages=find_packages(),
+      python_requires=">=3.3",
+      install_requires = get_requirements("requirements.txt"),
+      include_package_data=True,
+     )
