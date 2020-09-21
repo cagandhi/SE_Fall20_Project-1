daf53c9aa03128821472cba69a65e1d5f6fea501
oaaky <omkar.omkar.135@gmail.com>
2020-09-21 17:55:43 -0400

    updating the test

--- test/test_periodicLogSaver.py
+++ test/test_periodicLogSaver.py
@@ -11,3 +11,23 @@ periodicLoSaver = sys.modules["SE_Fall20_Project-1.SublimePlugin.periodicLogSave
 version = sublime.version()
 
 class TestPeriodicLogSaver(TestCase):
+
+	def test_write_log_file(self):
+	logger = periodicLogServer()
+	try:
+		d = {'2020-09-19': {'temp1.py': [[1000, 2000], [3000, 3200]]},
+			'2020-09-20': {'temp2.py': [[5000, 6000]]}}  # noqa: E128, E501
+
+		BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+		FILE_PATH = BASE_PATH + '/.temp_logs'
+
+		_ = logger.write_log_file(d, FILE_PATH)
+
+		arr = []
+		for local_date, file_dict in d.items():
+			for filenm, times_arr in file_dict.items():
+				for ele in times_arr:
+					str1 = local_date + ',' + filenm + ',' + str(ele[0]) + ',' + str(ele[1]) + '\n'  # noqa: E501
+					arr.append(str1)
+	finally:
+		os.remove(FILE_PATH)
