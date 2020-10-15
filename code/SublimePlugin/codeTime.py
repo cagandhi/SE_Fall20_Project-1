781e67b1c1a7d30918e74ccd17fef050c2f03dc3
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2020-10-15 19:17:38 -0400

    Updated second thread

--- code/SublimePlugin/codeTime.py
+++ code/SublimePlugin/codeTime.py
@@ -7,18 +7,19 @@ import sys
 import subprocess
 
 from .periodicLogSaver import PeriodicLogSaver
+from .periodicFileProcessor import PeriodicFileProcessor
 
 # create data folder based on OS
-if platform.system() == 'Windows':
-	DATA_FOLDER_PATH = os.path.join(os.getenv('APPDATA'), '.codeTime')
+if platform.system() == "Windows":
+    DATA_FOLDER_PATH = os.path.join(os.getenv("APPDATA"), ".codeTime")
 else:
-	DATA_FOLDER_PATH = os.path.join(os.path.expanduser('~'), '.codeTime')
+    DATA_FOLDER_PATH = os.path.join(os.path.expanduser("~"), ".codeTime")
 
 if not os.path.exists(DATA_FOLDER_PATH):
-	os.makedirs(DATA_FOLDER_PATH)
+    os.makedirs(DATA_FOLDER_PATH)
 
 # define log file path
-LOG_FILE_PATH = os.path.join(DATA_FOLDER_PATH, '.sublime_logs')
+LOG_FILE_PATH = os.path.join(DATA_FOLDER_PATH, ".sublime_logs")
 
 # define local variables
 file_times_dict = {}
@@ -27,113 +28,177 @@ periodic_log_save_on = True
 
 
 def when_activated(view):
-	try:
-		window = view.window()
-		if window is not None:
-			file_name = view.file_name()
+    try:
+        window = view.window()
+        if window is not None:
+            file_name = view.file_name()
 
-			if file_name is not None:
-				start_time = time.time()
-				end_time = None
+            if file_name is not None:
+                start_time = time.time()
+                end_time = None
 
-				curr_date = dt.now().strftime('%Y-%m-%d')
+                curr_date = dt.now().strftime("%Y-%m-%d")
 
-				if curr_date not in file_times_dict:
-					file_times_dict[curr_date] = {}
+                if curr_date not in file_times_dict:
+                    file_times_dict[curr_date] = {}
 
-				if file_name not in file_times_dict[curr_date]:
-					file_times_dict[curr_date][file_name] = [[start_time, end_time]]  # noqa: E501
-				else:
-					file_times_dict[curr_date][file_name].append([start_time, end_time])  # noqa: E501
+                if file_name not in file_times_dict[curr_date]:
+                    file_times_dict[curr_date][file_name] = [
+                        [start_time, end_time]
+                    ]  # noqa: E501
+                else:
+                    file_times_dict[curr_date][file_name].append(
+                        [start_time, end_time]
+                    )  # noqa: E501
 
-				print('File_name: ', file_name)
-				print('\n ----- \n')
-	except Exception as e:
-		exc_type, exc_obj, exc_tb = sys.exc_info()
-		print("codeTime:when_activated(): {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
+                print("File_name: ", file_name)
+                print("\n ----- \n")
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        print(
+            "codeTime:when_activated(): {error} on line number: {lno}".format(
+                error=str(e), lno=str(exc_tb.tb_lineno)
+            )
+        )  # noqa: E501
 
 
 def when_deactivated(view):
-	try:
-		window = view.window()
-		if window is not None:
-			file_name = view.file_name()
+    try:
+        window = view.window()
+        if window is not None:
+            file_name = view.file_name()
 
-			if file_name is not None:
-				end_time = time.time()
+            if file_name is not None:
+                end_time = time.time()
 
-				curr_date = dt.now().strftime('%Y-%m-%d')
+                curr_date = dt.now().strftime("%Y-%m-%d")
 
-				file_times_dict[curr_date][file_name][-1][1] = end_time
+                file_times_dict[curr_date][file_name][-1][1] = end_time
 
-				print('File_name: ', file_name)
-				print('\n ----- \n')
-	except Exception as e:
-		exc_type, exc_obj, exc_tb = sys.exc_info()
-		print("codeTime:when_deactivated(): {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
+                print("File_name: ", file_name)
+                print("\n ----- \n")
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        print(
+            "codeTime:when_deactivated(): {error} on line number: {lno}".format(
+                error=str(e), lno=str(exc_tb.tb_lineno)
+            )
+        )  # noqa: E501
 
 
 class CustomEventListener(sublime_plugin.EventListener):
-	def on_post_save(self, view):
-		print(view.file_name(), 'just got saved')
+    def on_post_save(self, view):
+        print(view.file_name(), "just got saved")
 
-	def on_activated(self, view):
-		try:
-			print(view.file_name(), 'is now the active view')
-			when_activated(view)
-		except Exception as e:
-			exc_type, exc_obj, exc_tb = sys.exc_info()
-			print("codeTime:CustomEventListener():on_activated() {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
+    def on_activated(self, view):
+        try:
+            print(view.file_name(), "is now the active view")
+            when_activated(view)
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print(
+                "codeTime:CustomEventListener():on_activated() {error} on line number: {lno}".format(
+                    error=str(e), lno=str(exc_tb.tb_lineno)
+                )
+            )  # noqa: E501
 
-	def on_deactivated(self, view):
-		try:
-			print(view.file_name(), 'is deactivated view')
-			when_deactivated(view)
-		except Exception as e:
-			exc_type, exc_obj, exc_tb = sys.exc_info()
-			print("codeTime:CustomEventListener():on_deactivated() {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
+    def on_deactivated(self, view):
+        try:
+            print(view.file_name(), "is deactivated view")
+            when_deactivated(view)
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print(
+                "codeTime:CustomEventListener():on_deactivated() {error} on line number: {lno}".format(
+                    error=str(e), lno=str(exc_tb.tb_lineno)
+                )
+            )  # noqa: E501
 
-	def on_close(self, view):
-		try:
-			print(view.file_name(), 'is no more')
+    def on_close(self, view):
+        try:
+            print(view.file_name(), "is no more")
 
-			file_name = view.file_name()
-			curr_date = dt.now().strftime('%Y-%m-%d')
-			if file_name is not None and file_name in file_times_dict[curr_date]:
-				end_time = time.time()
+            file_name = view.file_name()
+            curr_date = dt.now().strftime("%Y-%m-%d")
+            if (
+                file_name is not None
+                and file_name in file_times_dict[curr_date]
+            ):
+                end_time = time.time()
 
-				last_time_list = file_times_dict[curr_date][file_name][-1]
+                last_time_list = file_times_dict[curr_date][file_name][-1]
 
-				if last_time_list[1] is None:
-					last_time_list[1] = end_time
+                if last_time_list[1] is None:
+                    last_time_list[1] = end_time
 
-				with open(LOG_FILE_PATH, 'a') as f:
-					for _time in file_times_dict[curr_date][file_name]:
-						f.write(curr_date + ',' + file_name + ',' + str(_time[0]) + ',' + str(_time[1]) + '\n')  # noqa: E501
-				file_times_dict[curr_date].pop(file_name, None)
-		except Exception as e:
-			exc_type, exc_obj, exc_tb = sys.exc_info()
-			print("codeTime:CustomEventListener():on_close() {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
+                with open(LOG_FILE_PATH, "a") as f:
+                    for _time in file_times_dict[curr_date][file_name]:
+                        f.write(
+                            curr_date
+                            + ","
+                            + file_name
+                            + ","
+                            + str(_time[0])
+                            + ","
+                            + str(_time[1])
+                            + "\n"
+                        )  # noqa: E501
+                file_times_dict[curr_date].pop(file_name, None)
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print(
+                "codeTime:CustomEventListener():on_close() {error} on line number: {lno}".format(
+                    error=str(e), lno=str(exc_tb.tb_lineno)
+                )
+            )  # noqa: E501
 
 
 # view.run_command('dashboard')
 class DashboardCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		try:
-			print("Showing Graphs")
-			dir_path = os.path.dirname(os.path.realpath(__file__))
-			process = subprocess.Popen("/Users/prithvirajchaudhuri/Desktop/CSC510/Project/venv/bin/python3 '" + dir_path + "/output.py'", shell=True, stdout=subprocess.PIPE)  # noqa: E501, F841
-			# if the above line doesn't work, replace 'python3' with actual executable path of your python3 (for contributors)
-		except Exception as e:
-			exc_type, exc_obj, exc_tb = sys.exc_info()
-			print("codeTime:DashboardCommand():run() {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
+    def run(self, edit):
+        try:
+            print("Showing Graphs")
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            subprocess.Popen(
+                "/Users/prithvirajchaudhuri/Desktop/CSC510/Project/venv/bin/"
+                + "python3 '"
+                + dir_path
+                + "/output.py'",
+                shell=True,
+                stdout=subprocess.PIPE,
+            )  # noqa: E501, F841
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print(
+                "codeTime:DashboardCommand():run() {error} on line number: {lno}".format(
+                    error=str(e), lno=str(exc_tb.tb_lineno)
+                )
+            )  # noqa: E501
 
 
 def plugin_loaded():
-	try:
-		if periodic_log_save_on:
-			periodcLogSaver = PeriodicLogSaver(kwargs={'inMemoryLog': file_times_dict, 'timeout': periodic_log_save_timeout, 'LOG_FILE_PATH': LOG_FILE_PATH})  # noqa: E501
-			periodcLogSaver.start()
-	except Exception as e:
-		exc_type, exc_obj, exc_tb = sys.exc_info()
-		print("codeTime:plugin_loaded() {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
+    try:
+        if periodic_log_save_on:
+            periodcLogSaver = PeriodicLogSaver(
+                kwargs={
+                    "inMemoryLog": file_times_dict,
+                    "timeout": periodic_log_save_timeout,
+                    "LOG_FILE_PATH": LOG_FILE_PATH,
+                }
+            )  # noqa: E501
+            periodcLogSaver.start()
+            periodicFileProcessor = PeriodicFileProcessor(
+                kwargs={
+                    "inMemoryLog": file_times_dict,
+                    "timeout": periodic_log_save_timeout,
+                    "LOG_FILE_PATH": LOG_FILE_PATH,
+                }
+            )
+            periodicFileProcessor.start()
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        print(
+            "codeTime:plugin_loaded() {error} on line number: {lno}".format(
+                error=str(e), lno=str(exc_tb.tb_lineno)
+            )
+        )  # noqa: E501
--- code/SublimePlugin/periodicFileProcessor.py
+++ code/SublimePlugin/periodicFileProcessor.py
@@ -1,25 +1,46 @@
+import copy
+import sys
 import threading
 import time
-import copy
-import sublime
 from datetime import datetime as dt
-import sys
+
+import sublime
 
 
 class PeriodicFileProcessor(threading.Thread):
-
-    def __init__(self, group=None, target=None, name=None,
-					args=(), kwargs=None, verbose=None):
-		super(PeriodicFileProcessor, self).__init__(group=group, target=target, name=name)
-		self.args = args
-		self.kwargs = kwargs
-		return
+    def __init__(
+        self,
+        group=None,
+        target=None,
+        name=None,
+        args=(),
+        kwargs=None,
+        verbose=None,
+    ):
+        super(PeriodicFileProcessor, self).__init__(
+            group=group, target=target, name=name
+        )
+        self.args = args
+        self.kwargs = kwargs
+        return
 
     def run(self):
-		while True:
-			try:
-                #Add things here
-				time.sleep(self.kwargs['timeout'])
-			except Exception as e:
-				exc_type, exc_obj, exc_tb = sys.exc_info()
-				print("periodicLogSaver:PeriodicLogSaver:run(): {error} on line number: {lno}".format(error=str(e), lno=str(exc_tb.tb_lineno)))  # noqa: E501
+        while True:
+            try:
+                f = open(
+                    "/Users/prithvirajchaudhuri/Desktop/CSC510/"
+                    + "Project/CodeTime/demofile2.txt",
+                    "a",
+                )
+                f.write("Now the file has more content!")
+                f.close()
+                time.sleep(self.kwargs["timeout"])
+            except Exception as e:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                line_no = str(exc_tb.tb_lineno)
+                print(
+                    "periodicLogSaver:PeriodicLogSaver:run(): {error} on line \
+                    number: {lno}".format(
+                        error=str(e), lno=line_no
+                    )
+                )
