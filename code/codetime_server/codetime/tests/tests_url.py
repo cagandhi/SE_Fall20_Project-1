1da52659fd1a3409ff1cedf5fc417b144516c55f
Ayushi61 <ayushir47@gmail.com>
2020-10-25 12:52:13 -0400

    test urls

--- code/codetime_server/codetime/tests/tests_url.py
+++ code/codetime_server/codetime/tests/tests_url.py
@@ -3,16 +3,16 @@ from django.test import TestCase
 # Create your tests here.
 from django.test import SimpleTestCase
 from django.urls import resolve, reverse
-import codeTime.views
+import codetime.views
 # Create your tests here.
 
 
 class TestURLs(SimpleTestCase):
 
     def test_time_log_resolution(self):
-        url = reverse('timelog')
-        self.assertEquals(resolve(url).func.view_class, codeTime.views.TimeLogView)
+        url = reverse('timelog_url')
+        self.assertEquals(resolve(url).func.view_class, codetime.views.TimeLogView)
 
     def test_user_url_resolution(self):
-        url = reverse('user')
-        self.assertEquals(resolve(url).func.view_class, codeTime.views.UserView)
+        url = reverse('user_endpoint')
+        self.assertEquals(resolve(url).func.view_class, codetime.views.UserView)
