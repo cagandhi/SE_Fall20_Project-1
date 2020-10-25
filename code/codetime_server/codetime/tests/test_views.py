b61c2c6075e8159489bc6c6d52fbb695beeff057
Ayushi61 <ayushir47@gmail.com>
2020-10-25 18:47:50 -0400

    post_travis_fix

--- code/codetime_server/codetime/tests/test_views.py
+++ code/codetime_server/codetime/tests/test_views.py
@@ -7,6 +7,7 @@
 # Enter steps here
 from django.test import TestCase, Client
 from django.urls import reverse
+from codetime.models import User
 
 users_data = {
     "log_user_id": 123,
@@ -33,6 +34,6 @@ class TestPostViews(TestCase):
         """
         Test behaviour of correct POST request for creating a course workspace
         """
-        response = self.client.post(self.user_url, data=course_data, content_type='application/json')
+        response = self.client.post(self.user_url, data=users_data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(Course.objects.all().count(), 1)
+        self.assertEqual(User.objects.all().count(), 1)
