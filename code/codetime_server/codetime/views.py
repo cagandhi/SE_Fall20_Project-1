85cd842f94abe44f41d7bda71c68934f0091fbb1
adarshtri <adarsh.trivedi100@gmail.com>
2020-10-26 01:20:05 -0400

    Summary APIs

--- code/codetime_server/codetime/views.py
+++ code/codetime_server/codetime/views.py
@@ -1,36 +1,32 @@
 from rest_framework import generics
-from .request_handlers import handle_user_post, handle_get_file_logs, handle_log_file_post
+from .request_handlers import *
 from rest_framework.response import Response
 
 
 class UserView(generics.ListCreateAPIView, generics.RetrieveUpdateDestroyAPIView):
-    """
-    User View
-    """
-
+    
+    def get(self, request, *args, **kwargs):
+        response = handle_user_get(request)
+        return Response(data=response)
+    
     def post(self, request, *args, **kwargs):
-        """
-        User post request
-        """
         response = handle_user_post(request)
-        return Response(data=response, status=response.get("status", 201))
+        return Response(data=response)
 
 
 class TimeLogView(generics.ListAPIView, generics.CreateAPIView):
-    """
-    TimeLog View
-    """
-
+    
     def post(self, request, *args, **kwargs):
-        """
-        TimeLog post request
-        """
         response = handle_log_file_post(request)
-        return Response(data=response, status=response.get("status", 201))
+        return Response(data=response, status=200)
 
     def get(self, request, *args, **kwargs):
-        """
-        TimeLog get request
-        """
         response = handle_get_file_logs(request)
-        return Response(data=response, status=response.get("status", 200))
+        return Response(data=response, status=response.get('status', 200))
+
+
+class TimeLogSummaryView(generics.ListAPIView):
+    
+    def get(self, request, *args, **kwargs):
+        response = handle_summary_request(request)
+        return Response(data=response)
